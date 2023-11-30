5c1ade116c9119445bfc8bb62c031fe0c248b83a
Ahmad Zainul Abidin <ahmadclab@gmail.com>
2023-11-30 09:25:08 +0800

    Raised to 10 top stocks for msg

--- gapup.sh
+++ gapup.sh
@@ -2,5 +2,5 @@
 
 source /home/zainul/abdza/findvwap/venv/bin/activate
 /home/zainul/abdza/findvwap/gapup.py
-cp results.csv /home/zainul/abdza/telegram_ai_bot
+cp /home/zainul/abdza/findvwap/results.csv /home/zainul/abdza/telegram_ai_bot
 /home/zainul/abdza/telegram_ai_bot/send_message.sh
