7c1841b2ab707be37c6e857891979d90ea786b4d
Ahmad Zainul Abidin <ahmadclab@gmail.com>
2024-01-04 07:45:18 +0800

    Cleanuped up update stats

--- updatestats.sh
+++ updatestats.sh
@@ -5,8 +5,5 @@ source venv/bin/activate
 /home/zainul/abdza/findvwap/get_history.py
 /home/zainul/abdza/findvwap/cleanupraw.py
 /home/zainul/abdza/findvwap/analyze_raw.py
-/home/zainul/abdza/findvwap/get_history.py
-/home/zainul/abdza/findvwap/cleanupraw.py
-/home/zainul/abdza/findvwap/analyze_raw.py
 /home/zainul/abdza/findvwap/backtest.py
 /home/zainul/abdza/findvwap/ml.py
