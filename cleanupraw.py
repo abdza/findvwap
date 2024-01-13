72f2557ff46c2ba1cb0f699dbd91dd7a33beb2ca
Ahmad Zainul Abidin <ahmadclab@gmail.com>
2024-01-14 07:17:39 +0800

    Now will send pattern findings to telegram

--- cleanupraw.py
+++ cleanupraw.py
@@ -6,11 +6,17 @@ script_path = os.path.abspath(__file__)
 script_dir = os.path.dirname(script_path)
 
 datas = pd.read_csv(os.path.join(script_dir,'raw_data.csv'))
+datas.dropna(inplace=True)
 datas.drop_duplicates(subset=['ticker','date'],inplace=True)
+# datas = datas.loc[datas['2 Days Start']!='']
+# datas = datas.loc[datas['2 Days Start']!=' ']
+# datas = datas.loc[datas['2 Days Start']!=None]
 datas.to_csv(os.path.join(script_dir,'raw_data.csv'),index=False)
 datas = pd.read_csv(os.path.join(script_dir,'raw_data_perc.csv'))
+datas.dropna(inplace=True)
 datas.drop_duplicates(subset=['ticker','date'],inplace=True)
 datas.to_csv(os.path.join(script_dir,'raw_data_perc.csv'),index=False)
 datas = pd.read_csv(os.path.join(script_dir,'raw_data_perc_marks.csv'))
+datas.dropna(inplace=True)
 datas.drop_duplicates(subset=['ticker','date'],inplace=True)
 datas.to_csv(os.path.join(script_dir,'raw_data_perc_marks.csv'),index=False)
--- gapup.sh
+++ gapup.sh
@@ -4,6 +4,6 @@ source /home/zainul/abdza/findvwap/venv/bin/activate
 /home/zainul/abdza/findvwap/gapup.py
 deactivate
 now=`date +"%Y-%m-%d-%H-%M"`
-mv /home/zainul/abdza/telegram_ai_bot/results_profitability.csv "/home/zainul/abdza/telegram_ai_bot/result_profitabilty_${now}.csv"
-cp /home/zainul/abdza/findvwap/results_profitability.csv /home/zainul/abdza/telegram_ai_bot
+cp /home/zainul/abdza/findvwap/results_profitability.csv "/home/zainul/abdza/telegram_ai_bot/result_profitabilty_${now}.csv"
+cp /home/zainul/abdza/findvwap/results_profitability.csv /home/zainul/abdza/telegram_ai_bot/
 /home/zainul/abdza/telegram_ai_bot/send_message.sh
